8b8226b38612e6e4e1c8ded562da5532365aee4c
LucaWinds <113988613+LucaWinds@users.noreply.github.com>
2023-04-25 09:36:58 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <numeric>
+
+using namespace std;
+
+typedef long long int ll;
+
+const ll mod = 1000000007;
+
+int arr[8];
+bool visited[8];
+vector<int> res;
+int N, M;
+
+void DFS(int x) {
+    if (x == M) {
+        for (int i = 0; i < M; i++) {
+            cout << res[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    int pre = -1;
+
+    for (int i = 0; i < N; i++) {
+        if (visited[i] || pre == arr[i]) continue;
+
+        visited[i] = true;
+        pre = arr[i];
+        res.push_back(arr[i]);
+        DFS(x + 1);
+        res.pop_back();
+        visited[i] = false;
+    }
+}
+
+int main() {
+
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + N);
+
+    memset(visited, false, sizeof(visited));
+    DFS(0);
+
+    return 0;
+}
