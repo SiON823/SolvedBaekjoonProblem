a1686c82339cadfbdaf9ccaf0c6066dee17e87ee
LucaWinds <113988613+LucaWinds@users.noreply.github.com>
2023-04-25 09:35:55 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <numeric>
+
+using namespace std;
+
+typedef long long int ll;
+
+const ll mod = 1000000007;
+
+vector<int> res;
+int N, M;
+
+void DFS(int num, int x) {
+    if (x == M) {
+        for (int i = 0; i < M; i++) {
+            cout << res[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = num; i < N; i++) {
+        res.push_back(i+1);
+        DFS(i+1, x+1);
+        res.pop_back();
+    }
+}
+
+int main() {
+    
+    cin >> N >> M;
+
+    DFS(0, 0);
+
+    return 0;
+}
