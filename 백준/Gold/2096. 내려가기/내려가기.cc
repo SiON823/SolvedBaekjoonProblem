9f20013bac35f8f372bd780a299b257f0521ac96
LucaWinds <113988613+LucaWinds@users.noreply.github.com>
2023-04-25 09:01:25 +0900

    [Gold V] Title: 내려가기, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.cc
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long int ll;
+
+
+int max_arr[2][3];
+int min_arr[2][3];
+
+int main() {
+	int N;
+	int x, y, z;
+	int	k = 0;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> x >> y >> z;
+
+		max_arr[k][0] = max(max_arr[1 - k][0], max_arr[1 - k][1]) + x;
+		max_arr[k][1] = max({ max_arr[1 - k][0], max_arr[1 - k][1], max_arr[1 - k][2] }) + y;
+		max_arr[k][2] = max(max_arr[1 - k][1], max_arr[1 - k][2]) + z;
+
+		min_arr[k][0] = min(min_arr[1 - k][0], min_arr[1 - k][1]) + x;
+		min_arr[k][1] = min({ min_arr[1 - k][0], min_arr[1 - k][1], min_arr[1 - k][2] }) + y;
+		min_arr[k][2] = min(min_arr[1 - k][1], min_arr[1 - k][2]) + z;
+
+		k = 1 - k;
+	}
+
+	cout << max({ max_arr[1 - k][0], max_arr[1 - k][1], max_arr[1 - k][2] }) << ' '
+		<< min({ min_arr[1 - k][0], min_arr[1 - k][1], min_arr[1 - k][2] }) << '\n';
+
+	return 0;
+}
