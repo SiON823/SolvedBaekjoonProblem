f12a702fd8e634365281fa09aef8344a0779592e
LucaWinds <113988613+LucaWinds@users.noreply.github.com>
2023-05-05 10:56:47 +0900

    [Silver IV] Title: 피하자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/25379. 피하자/피하자.cc
+++ 백준/Silver/25379. 피하자/피하자.cc
@@ -10,26 +10,24 @@ typedef long long int ll;
 
 
 int main() {
-	vector<int> arr;
-	vector<int> idx1, idx2;
+	vector<ll> idx1, idx2;
 
-	int N;
+	ll N;
 	cin >> N;
 
 	for (int i = 0; i < N; i++) {
-		int x;
+		ll x;
 		cin >> x;
-		arr.push_back(x%2);
 
 		if (x % 2 == 0) idx1.push_back(i);
 		else idx2.push_back(i);
 	}
 
-	int sum1 = accumulate(idx1.begin(), idx1.end(), 0);
-	int sum2 = accumulate(idx2.begin(), idx2.end(), 0);
+	ll sum1 = accumulate(idx1.begin(), idx1.end(), 0);
+	ll sum2 = accumulate(idx2.begin(), idx2.end(), 0);
 
-	int size1 = idx1.size();
-	int size2 = idx2.size();
+	ll size1 = idx1.size();
+	ll size2 = idx2.size();
 
 	cout << min(-((size1 * (size1 - 1)) / 2) + sum1, -((size2 * (size2 - 1)) / 2) + sum2) << '\n';
 
