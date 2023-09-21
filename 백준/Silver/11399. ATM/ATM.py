8d9f729df6599a230dcf7f4799d51a081a7aa265
Fly'nChihwahwa <113988613+LucaWinds@users.noreply.github.com>
2023-09-21 09:16:14 +0900

    [Silver IV] Title: ATM, Time: 124 ms, Memory: 115004 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,9 +1,19 @@
-# 11399
-# ATM
+def insertionSort(arr):
+    # 1 ~ n-1까지 요소가 삽입할 요소
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        # 삽입할 요소보다 큰 요소는 한 칸씩 뒤로 밀어냄
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        # 삽입할 요소를 삽입
+        arr[j + 1] = key
 
 n = int(input())
 a = list(map(int, input().split()))
-a.sort()
+insertionSort(a)
+
 result = 0
 for i in range(n):
     result += sum(a[:i+1])
